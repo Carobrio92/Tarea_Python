f7bf2b3d09f190d063caa6dbae0eafc09dbf4961
Carolina <marybi9012@outlook.com>
2023-10-08 20:20:33 +0200

    agregar primera tarea

---
+++ TAREA 1 PYTHON/Deberes.py
@@ -0,0 +1,24 @@
+#---tarea de Python---
+install.packages("reticulate")
+import pandas as pd
+pd.DataFrame({'a':[1]})
+import glob
+glob.glob('*')
+import matplotlib.pyplot as plt
+type("Carolina")
+type("Briones")
+type(int(30))
+dict = {"a": "Carolina", "b": "Briones","c":30}
+dict["c"]
+my_lista=[1,2,3]
+
+
+dict = {"a": ["Carolina"], "b": ["Briones"],"c":[30]}
+df= pd.DataFrame(dict)
+df
+dict
+dict.keys()
+dict.values()
+for i in dict.values():
+  print(i)
+
